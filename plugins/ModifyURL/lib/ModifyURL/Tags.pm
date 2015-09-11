b9dbec1578c895805d8068efb91e6a060d3b3767
Hideki Abe <hideki.abe.0630@gmail.com>
2015-09-12 08:10:33 +0900

    spモディファイアの処理でウェブサイトURLを考慮するよう変更

--- plugins/ModifyURL/lib/ModifyURL/Tags.pm
+++ plugins/ModifyURL/lib/ModifyURL/Tags.pm
@@ -1,5 +1,6 @@
 package ModifyURL::Tags;
 use strict;
+use MT::Blog;
 
 sub _hdlr_relativeurl {
     my ($str, $arg, $ctx) = @_;
@@ -14,10 +15,22 @@ sub _hdlr_smartphone {
     my ($str, $arg, $ctx) = @_;
     return $str if $arg != 1;
 
-    if ($str =~ /^http/) {
-        $str =~ s/http(s)?:\/\/([^\/]+)/http$1:\/\/$2\/sp/;
+    my $blog = $ctx->stash('blog') || return;
+    my $website_id = $blog->parent_id || 0;
+    my $website_url;
+
+    if ($website_id == 0) {
+        $website_url = $blog->site_url;
     } else {
-        $str = '/sp' . $str;
+        my $website = MT::Blog->load($website_id) || return;
+        $website_url = $website->site_url;
+    }
+
+    if ($str =~ /^http/) {
+        $str =~ s/($website_url)/$1sp\//;
+    } else {
+        (my $path = $website_url) =~ s/https?:\/\/[^\/]+//;
+        $str =~ s/($path)/$1sp\// . $str;
     }
 
     return $str;
--- plugins/ModifyURL/php/modifier.sp.php
+++ plugins/ModifyURL/php/modifier.sp.php
@@ -5,10 +5,29 @@ function smarty_modifier_sp($str, $args)
         return $str;
     }
 
-    if (strpos($str, 'http') === FALSE) {
-        $str = '/sp' . $str;
+    $mt = MT::get_instance();
+    $ctx =& $mt->context();
+    $websiteId = $ctx->__stash['blog']->blog_parent_id || 0;
+
+    if ($websiteId) {
+        $website = $mt->db()->fetch_blog($websiteId);
+        $websiteUrl = $website->site_url;
     } else {
-        $str = preg_replace('/http(s)?:\/\/([^\/]+)/', 'http$1://$2/sp', $str);
+        $websiteUrl = $ctx->__stash['blog']->site_url;
+    }
+
+    if (strpos($str, 'http') === FALSE) {
+        $path = preg_replace('/https?:\/\/[^\/]+/', '', $websiteUrl);
+
+        if ($path === '/') {
+            $str = $path . 'sp' . $str;
+        } else if (preg_match('/\/$/', $path)) {
+            $str = $path . 'sp/' . str_replace($path, '', $str);
+        } else {
+            $str = '/sp' . $str;
+        }
+    } else {
+        $str = str_replace($websiteUrl, $websiteUrl . 'sp/', $str);
     }
 
     return $str;
